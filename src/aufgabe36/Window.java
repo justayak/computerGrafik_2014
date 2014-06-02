1a110257ebdbb8c7a27c8e89c370b240b20826c8
julian <jutanke@googlemail.com>
2014-06-02 23:58:59 +0200

    ellipse yay ... Diophantische Gleichung

--- src/aufgabe36/Window.java
+++ src/aufgabe36/Window.java
@@ -13,27 +13,32 @@ public class Window extends JPanel {
     private static final int HALF_WIDTH = WIDTH / 2;
     private static final int HALF_HEIGHT = HEIGHT / 2;
 
-    double a = -1;
-    double b = 0.1;
-    double c = 1;
-    double d = -0.75;
-    double e = 0.13;
-    double f = 0.5;
+    int a = 42;
+    int b =8;
+    int c = 15;
+    int d = 23;
+    int e = 17;
+    int f = -4915;
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         g.setColor(Color.DARK_GRAY);
 
+        double aQ = 250;
+        double bQ = 200;
+        double xM = -20;
+        double yM = -20;
+
         for(int x = -HALF_WIDTH;x < HALF_WIDTH; x++ ){
             for(int y = -HALF_HEIGHT;y < HALF_HEIGHT; y++){
-                //double v = a*x*x + b*x*y + c*y*y + d*x + e*y + f;
 
-                double a = 250;
-                double b = 200;
-                double v = Math.ceil(((x*x) / (a*a)) + ((y*y)/(b*b)));
+                int v = a*x*x + b*x*y + c*y*y + d*x + e*y + f;
+                //double v = Math.ceil((Math.pow(x-xM,2) / (a*a)) + (Math.pow(y-yM,2)/(b*b)));
 
-                if (v  == 1){
+                //double v = (a*Math.pow(x,2)) + (b*x*y) + (c*Math.pow(y,2)) + (d*x) + (e*y) + f;
+
+                if (v  <= 0){
                     g.fillRect(x+HALF_WIDTH,y+HALF_HEIGHT,1,1);
                 }
             }
