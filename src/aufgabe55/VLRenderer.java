fbafd31ba066ec6aadf290662d7bead0376b0403
julian <jutanke@googlemail.com>
2014-07-06 19:06:03 +0200

    urks... bad

---
+++ src/aufgabe55/VLRenderer.java
@@ -0,0 +1,37 @@
+package aufgabe55;
+
+import com.jogamp.opengl.swt.GLCanvas;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+/**
+ * Created by Julian on 06.07.2014.
+ * Renderer aus der Vorlesung..
+ */
+public class VLRenderer implements GLEventListener {
+
+    static GLU glu = new GLU();
+    //static GLCanvas canvas = new GLCanvas();
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i2, int i3, int i4) {
+
+    }
+}
